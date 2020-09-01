7540ba1fcb1546e4e7dbb3f2d82fbb136d875563
Ahmed Yousseef <ahmed.yousseef.7@gmail.com>
2020-09-01 11:19:28 +0200

    AddRole

--- SmartSite/Startup.cs
+++ SmartSite/Startup.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SmartSite.Models;
 
 [assembly: OwinStartupAttribute(typeof(SmartSite.Startup))]
 namespace SmartSite
 {
     public partial class Startup
     {
+        ApplicationDbContext DB;
+        public Startup()
+        {
+            DB = new ApplicationDbContext();
+        }
+
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
         }
+
+        public void CreateRole()
+        {
+            //RoleManager roleManager = new RoleManager<IdentityRole>() { };
+        }
+
     }
 }
